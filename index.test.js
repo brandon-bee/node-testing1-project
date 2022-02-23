7031db13438de4bf7199d091a93a08eef7124d0d
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-23 14:30:59 -0800

    Finished Exercise 4

--- index.test.js
+++ index.test.js
@@ -43,13 +43,18 @@ describe('[Exercise 4] Counter', () => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
   test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
-    expect(true).toBe(false)
+    expect(counter.countDown()).toBe(3)
   })
   test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
-    expect(true).toBe(false)
+    counter.countDown()
+    expect(counter.countDown()).toBe(2)
   })
   test('[8] the count eventually reaches zero but does not go below zero', () => {
-    expect(true).toBe(false)
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    expect(counter.countDown()).toBe(0)
   })
 })
 
