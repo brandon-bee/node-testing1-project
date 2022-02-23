64baf6397042304f4e7c7c541ed97b2c6e8dafae
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-23 13:58:33 -0800

    Finished Exercise 1

--- index.test.js
+++ index.test.js
@@ -8,16 +8,26 @@ describe('[Exercise 1] trimProperties', () => {
     const actual = utils.trimProperties(input)
     expect(actual).toEqual(expected)
   })
-  // test('[2] returns a copy, leaving the original object intact', () => {})
+  test('[2] returns a copy, leaving the original object intact', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    utils.trimProperties(input)
+    expect(input).toEqual({ foo: '  foo ', bar: 'bar ', baz: ' baz' })
+  })
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
-  // test('[3] returns an object with the properties trimmed', () => {})
-  // test('[4] the object returned is the exact same one we passed in', () => {})
+  test('[3] returns an object with the properties trimmed', () => {
+    expect(true).toBe(false)
+  })
+  test('[4] the object returned is the exact same one we passed in', () => {
+    expect(true).toBe(false)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    expect(true).toBe(false)
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
@@ -25,9 +35,15 @@ describe('[Exercise 4] Counter', () => {
   beforeEach(() => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
-  // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
+    expect(true).toBe(false)
+  })
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    expect(true).toBe(false)
+  })
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    expect(true).toBe(false)
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
@@ -35,12 +51,24 @@ describe('[Exercise 5] Seasons', () => {
   beforeEach(() => {
     seasons = new utils.Seasons() // each test must start with fresh seasons
   })
-  // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
-  // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
-  // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
-  // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
-  // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
-  // test('[14] the 40th call of seasons.next returns "spring"', () => {})
+  test('[9] the FIRST call of seasons.next returns "summer"', () => {
+    expect(true).toBe(false)
+  })
+  test('[10] the SECOND call of seasons.next returns "fall"', () => {
+    expect(true).toBe(false)
+  })
+  test('[11] the THIRD call of seasons.next returns "winter"', () => {
+    expect(true).toBe(false)
+  })
+  test('[12] the FOURTH call of seasons.next returns "spring"', () => {
+    expect(true).toBe(false)
+  })
+  test('[13] the FIFTH call of seasons.next returns again "summer"', () => {
+    expect(true).toBe(false)
+  })
+  test('[14] the 40th call of seasons.next returns "spring"', () => {
+    expect(true).toBe(false)
+  })
 })
 
 describe('[Exercise 6] Car', () => {
@@ -48,13 +76,25 @@ describe('[Exercise 6] Car', () => {
   beforeEach(() => {
     focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
   })
-  // test('[15] driving the car returns the updated odometer', () => {})
-  // test('[16] driving the car uses gas', () => {})
-  // test('[17] refueling allows to keep driving', () => {})
-  // test('[18] adding fuel to a full tank has no effect', () => {})
+  test('[15] driving the car returns the updated odometer', () => {
+    expect(true).toBe(false)
+  })
+  test('[16] driving the car uses gas', () => {
+    expect(true).toBe(false)
+  })
+  test('[17] refueling allows to keep driving', () => {
+    expect(true).toBe(false)
+  })
+  test('[18] adding fuel to a full tank has no effect', () => {
+    expect(true).toBe(false)
+  })
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', () => {
+    expect(true).toBe(false)
+  })
+  test('[20] resolves false if passed an odd number', () => {
+    expect(true).toBe(false)
+  })
 })
